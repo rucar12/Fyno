82dc334bae117f0fb9cdb37c1f49c82fb447d874
rucar12 <vasya19956a@gmail.com>
2024-06-07 19:25:32 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Fyno/',
+    base: './',
     plugins: [
         react(),
         svgr({
